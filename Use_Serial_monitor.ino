7609dddf90ed6865bff1ee096fd046e3b4477cca
HThBao <137778579+HThBao@users.noreply.github.com>
2023-11-17 16:33:30 +0700

    Update Use_Serial_monitor.ino

--- Use_Serial_monitor.ino
+++ Use_Serial_monitor.ino
@@ -1,45 +1,54 @@
-/*Example read data from serial monitor
-char buffer[4]= {}; // Biến đệm lưu giá trị
-void setup() {
-        Serial.begin(9600);     // mở serial với baudrate 9600
-}
 
-void loop() {
-
-        // nếu còn có thể đọc được 
-        if (Serial.available() >= 3) {
-                // đọc chữ liệu
-                Serial.readBytes(buffer,3);
-                //Xuất dữ liệu
-                Serial.print("Toi nhan duoc: ");
-                Serial.println(buffer);
-        }
-} 
-*/
-/*
- * This code use serial monitor to control MCU 
- * Data may be char or string 
- * 
- */
+  char check_ID[3];
+  char Led_ID[3];
+  char status_led[3];
 void setup()
 {
   Serial.begin(9600);
   pinMode(8, OUTPUT);
   digitalWrite(8, LOW); // lúc đầu ta để LED tắt
 }
+
 void loop()
 {
-  if(Serial.available()) { // nếu có dữ liệu gửi đến
-   String text = Serial.readStringUntil('\n'); // đọc giá trị gửi đến cho đến khi gặp kí tự xuống dòng \n
+    if(Serial.available()) { // nếu có dữ liệu gửi đến
+    String data = Serial.readStringUntil('\n'); // đọc giá trị gửi đến cho đến khi gặp kí tự xuống dòng \n
+    String data_id = data.substring(0,2);
+    String data_id_device = data.substring(2,4);
+    Serial.print("data_id: ");
+    Serial.println(data_id);
+
+      Serial.print("data_id_device: ");
+    Serial.println(data_id_device);
     
-    if(text == "ON") {
-        digitalWrite(8,HIGH); // bật LED
-        Serial.println("LED is turned ON");  // gửi lại thông báo lên Serial Monitor
-    }
+//    if(text == "0000") {
+//        digitalWrite(8,HIGH); // bật LED
+//        Serial.println("LED Green is turned ON");  // gửi lại thông báo lên Serial Monitor
+//    }
+//    
+//    if(text == "0001") {
+//        digitalWrite(8,LOW); // tắt LED
+//        Serial.println("LED Green is turned OFF");
+//    }
+//
+//      if(text == "0100") {
+//        digitalWrite(9,HIGH); // bật LED
+//        Serial.println("LED Red is turned ON");  // gửi lại thông báo lên Serial Monitor
+//    }
+//    
+//    if(text == "0101") {
+//        digitalWrite(9,LOW); // tắt LED
+//        Serial.println("LED Red is turned OFF");
+//    }
+//        if(text == "0200") {
+//        digitalWrite(10,HIGH); // bật LED
+//        Serial.println("LED Yellow is turned ON");  // gửi lại thông báo lên Serial Monitor
+//    }
+//    
+//    if(text == "0201") {
+//        digitalWrite(10,LOW); // tắt LED
+//        Serial.println("LED Yellow is turned OFF");
+//    }
     
-    if(text == "OFF") {
-        digitalWrite(8,LOW); // tắt LED
-        Serial.println("LED is turned OFF");
-    }
    }
  }
